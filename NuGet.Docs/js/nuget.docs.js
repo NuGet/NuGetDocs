fba7f5661245484890a3cece793d13450929e92b
Maarten Balliauw <maarten@maartenballiauw.be>
2015-02-16 15:43:23 +0100

    Added responsive menu to the docs

--- NuGet.Docs/js/nuget.docs.js
+++ NuGet.Docs/js/nuget.docs.js
@@ -5,4 +5,34 @@
         $("#doc").css("margin-right", "0px");
         return false;
     });
+
+    // -- responsive menu --
+    $('.nav-toggle').on('click', function (e) {
+        $target = $($(this).data('toggle'));
+        if ($target) {
+            $target.slideToggle();
+        }
+
+        $('.nav-toggle').each(function (i, item) {
+            $ele = $($(item).data('toggle'));
+            if ($ele && !$ele.is($target)) {
+                $ele.slideUp();
+            }
+        });
+        e.preventDefault();
+    });
+
+    $(window).on('resize', function () {
+        var w = $(window).width();
+
+        $('.nav-toggle').each(function (i, item) {
+            $target = $($(item).data('toggle'));
+            if ($target && w > 600) {
+                $target.show();
+            } else if ($target && w <= 600 && $target.is(':visible')) {
+                $target.slideUp();
+            }
+        });
+    });
+    // -- / responsive menu --
 });
