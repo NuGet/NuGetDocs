2f73fce9d01d47486c7924d7d60f472ca9944159
Harikrishna Menon <harikm@microsoft.com>
2016-06-26 08:38:44 -0700

    Basic toc

--- NuGet.Docs/js/nuget.docs.js
+++ NuGet.Docs/js/nuget.docs.js
@@ -36,3 +36,35 @@
     });
     // -- / responsive menu --
 });
+
+function prepareList() {
+    $('#expList').find('li:has(ul)')
+    .click( function(event) {
+        if (this == event.target) {
+            $(this).toggleClass('expanded');
+            $(this).children('ul').toggle('medium');
+        }
+        return false;
+    })
+    .addClass('collapsed')
+    .children('ul').hide();
+
+    //Create the button funtionality
+    $('#expandList')
+    .unbind('click')
+    .click( function() {
+        $('.collapsed').addClass('expanded');
+        $('.collapsed').children().show('medium');
+    })
+    $('#collapseList')
+    .unbind('click')
+    .click( function() {
+        $('.collapsed').removeClass('expanded');
+        $('.collapsed').children().hide('medium');
+    })
+    
+};
+
+$(document).ready(function () {
+    prepareList()
+});
